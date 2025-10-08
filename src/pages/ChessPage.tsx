2e61da3427f07a67537a62ff81e795c74b8ac14a
Peter Kerfoot <peterckerfoot@gmail.com>
2025-10-08 16:54:11 -0500

    added header to ChessPage and formatted

--- src/pages/ChessPage.tsx
+++ src/pages/ChessPage.tsx
@@ -1,7 +1,15 @@
 // displays chessboard game
+import Header from '../components/Header'
 
 const ChessPage = () => {
-  return <div>ChessPage</div>
+  return (
+    <div className='min-h-screen flex flex-col'>
+      <Header />
+      <div className='flex-1 flex items-center justify-center'>
+        <h1 className='text-3xl font-bold'>ChessPage</h1>
+      </div>
+    </div>
+  )
 }
 
 export default ChessPage
